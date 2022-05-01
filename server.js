76d92c84480c052fa6b2a066c19bf3dc8062a838
Leyenda Nerd <66542754+leyendanerd@users.noreply.github.com>
2022-05-01 00:03:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Julio Montero');
+  res.send('Hello World Julio Montero with terraform and github action');
 });
 
 app.listen(PORT, HOST);
