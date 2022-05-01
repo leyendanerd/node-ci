1e0879e33ad92c22ec13499cd09133ea6971e7d0
leyendanerd <leyendanerd@gmail.com>
2022-05-01 01:32:15 -0400

    red

--- do-terraform/02_droplet.tf
+++ do-terraform/02_droplet.tf
@@ -5,5 +5,9 @@ resource "digitalocean_droplet" "web" {
   region = "nyc1"
   size   = "s-1vcpu-1gb"
   user_data = file("setup.yaml")
+  floating_ip = "${digitalocean_floating_ip.web.ip_address}"
 }
 
+resource "digitalocean_floating_ip" "web" {
+    region = "nyc1"
+}
