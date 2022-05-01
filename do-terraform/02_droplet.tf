442f78ddde76952c0303cf452155c0b61e275c62
leyendanerd <leyendanerd@gmail.com>
2022-05-01 01:39:38 -0400

    red

--- do-terraform/02_droplet.tf
+++ do-terraform/02_droplet.tf
@@ -1,13 +1,17 @@
+resource "digitalocean_floating_ip" "web" {
+  region = "nyc1"
+}
+
 # Create a new Web Droplet in the nyc1 region
 resource "digitalocean_droplet" "web" {
   image  = "docker-18-04"
-  name   = "web-1"
+  name   = "web"
   region = "nyc1"
   size   = "s-1vcpu-1gb"
   user_data = file("setup.yaml")
-  floating_ip = "${digitalocean_floating_ip.web.ip_address}"
 }
 
-resource "digitalocean_floating_ip" "web" {
-    region = "nyc1"
+resource "digitalocean_floating_ip_assignment" "web" {
+  ip_address = digitalocean_floating_ip.web.ip_address
+  droplet_id = digitalocean_droplet.web.id
 }
