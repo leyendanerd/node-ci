96e73d184a52c5dae3505e1e0ee8ed30ff3045d1
Leyenda Nerd <66542754+leyendanerd@users.noreply.github.com>
2022-05-18 23:28:20 -0400

    Update variables.tf

--- do-terraform/variables.tf
+++ do-terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "do_token" {
  type= string
  description= "Token"
- default= "dop_v1_be4ad5442efce6995199ff4abf61ef25d712413b4de3b652f6f4736c045e7084"
+ default= "dop_v1_be4ad5442efce6995199ff4abf61ef25d712413b4de3b652f6f4736c045e708"
 
 }
 
